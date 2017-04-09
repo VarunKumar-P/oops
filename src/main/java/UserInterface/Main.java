6684eb773b9ae6c89be7877a84fa7febecea69ed
varun <trulyvarunkumar@gmail.com>
2017-04-09 19:22:26 +0530

    temp change

--- src/main/java/UserInterface/Main.java
+++ src/main/java/UserInterface/Main.java
@@ -5,6 +5,6 @@ package UserInterface;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("lalala");
+        System.out.println("lalalala");
     }
 }
